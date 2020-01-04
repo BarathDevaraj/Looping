45d5ff801ec0cfc856a0f4b13062784ecfc5ed4e
Barath Devaraj <52287990+BarathDevaraj@users.noreply.github.com>
2020-01-04 18:46:02 +0530

    Create SumOfN_NaturalNumbers.py

---
+++ SumOfN_NaturalNumbers.py
@@ -0,0 +1,7 @@
+n=int(input())
+#print(int(n*(n+1)/2))
+#above print statement is formula methodin which output can be obtain in single line code
+sum=0
+for i in range(1,n+1):
+    sum=sum+i
+print(sum)
